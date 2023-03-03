fb107effb45e378a852bde7610437241ee40881a
Amelia Walsh <ameliatwalsh@gmail.com>
2023-03-03 15:48:53 -0800

    added remaining object properties

--- student_application/lib/models/project.dart
+++ student_application/lib/models/project.dart
@@ -6,20 +6,26 @@ class Project {
   String ProjectType = '';
   String ProjectDescription = '';
   String ProjectImage = '';
+  int AccessCode;
+  String ProjectInstructions = '';
 
-  Project({
-    required this.IDProjects,
-    required this.ProjectName,
-    required this.ProjectType,
-    required this.ProjectDescription,
-    required this.ProjectImage
-  });
+  Project(
+      {required this.IDProjects,
+      required this.ProjectName,
+      required this.ProjectType,
+      required this.ProjectDescription,
+      required this.ProjectImage,
+      required this.AccessCode,
+      required this.ProjectInstructions
+      });
 
   factory Project.fromJson(Map<String, dynamic> json) => Project(
       IDProjects: json["IDUser"],
       ProjectName: json["ProjectName"],
       ProjectType: json["ProjectType"],
       ProjectDescription: json["ProjectDescription"],
-      ProjectImage: json["ProjectImage"]
+      ProjectImage: json["ProjectImage"],
+      AccessCode: json["AccessCode"],
+      ProjectInstructions: json["ProjectInstructions"]
   );
 }
--- student_application/lib/models/student.dart
+++ student_application/lib/models/student.dart
@@ -4,17 +4,23 @@ class Student {
   int IDUser;
   String FirstName = '';
   String LastName = '';
+  String Email = '';
+  int IsTeacher;
 
   Student({
     required this.IDUser, 
     required this.FirstName, 
-    required this.LastName
+    required this.LastName,
+    required this.Email,
+    required this.IsTeacher
   });
 
   factory Student.fromJson(Map<String, dynamic> json) => Student(
       IDUser: json["IDUser"],
       FirstName: json["FirstName"],
-      LastName: json["LastName"]
+      LastName: json["LastName"],
+      Email: json["Email"],
+      IsTeacher: json["IsTeacher"]
   );
   
 }
