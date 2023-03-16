6a74c5205d970666c0d436b3480656706ae8d633
Amelia Walsh <ameliatwalsh@gmail.com>
2023-03-16 09:33:21 -0700

    Input validation, skeleton for image picker

--- student_application/lib/screens/project-entry-screen.dart
+++ student_application/lib/screens/project-entry-screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -17,95 +16,173 @@ class ProjectEntryScreen extends StatefulWidget {
   }
 }
 
-// Create a corresponding State class.
-// This class holds data related to the form.
 class ProjectEntryScreenState extends State<ProjectEntryScreen> {
-  // Create a global key that uniquely identifies the Form widget
-  // and allows validation of the form.
-  //
-  // Note: This is a GlobalKey<FormState>,
-  // not a GlobalKey<ProjectEntryScreenState>.
   final _formKey = GlobalKey<FormState>();
+
+  String _IDUsers = '';
+  String _latitude = '';
+  String _longitude = '';
+
+  void _onSubmit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState?.save(); // Save form
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     List<DropdownMenuItem<String>> dropdownList = [];
-    widget.studentList.forEach(
-      (element) => {
-        dropdownList.add(
-          DropdownMenuItem<String>(child: Text(element.FirstName + ' ' + element.LastName), value: element.IDUser.toString()),
-        )
-      }
-    );
+    widget.studentList.forEach((element) => {
+          dropdownList.add(
+            DropdownMenuItem<String>(
+                child: Text(element.FirstName + ' ' + element.LastName),
+                value: element.IDUser.toString()),
+          )
+        });
     // Build a Form widget using the _formKey created above.
     return SafeArea(
       child: Center(
-        child: Form(
-            key: _formKey,
-            child: ListView(
-              padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-              children: <Widget>[
-                 ListTile(
+        child: Card(
+          margin: EdgeInsets.all(20.0),
+          child: Form(
+              key: _formKey,
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                child: ListView(
+                  children: <Widget>[
+                    ListTile(
                       leading: Text(
                         'What did you find?',
                         style: Theme.of(context).textTheme.headlineMedium,
-                        ),
-                    ),
-                  SizedBox(
-                    width: 20,
-                    height: 20,
-                  ),
-                  DropdownButtonFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Select a Student', 
-                        border: OutlineInputBorder()
                       ),
-                      items: dropdownList,
-                      onChanged: (_){}
                     ),
-                  SizedBox(
-                    width: 20,
-                    height: 20,
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: Row(
+                    SizedBox(
+                      width: 20,
+                      height: 20,
+                    ),
+                    DropdownButtonFormField(
+                        decoration: InputDecoration(
+                            labelText: 'Select a Student',
+                            border: OutlineInputBorder()),
+                        items: dropdownList,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Select a student.';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            _IDUsers = value.toString();
+                         });
+                        },
+                    ),
+                    SizedBox(
+                      width: 20,
+                      height: 20,
+                    ),
+                    Row(
                       children: [
-                        Flexible(
+                        Expanded(
                           child: TextFormField(
-                            keyboardType: TextInputType.numberWithOptions(decimal: true),
+                            keyboardType: TextInputType.numberWithOptions(
+                                signed: true, decimal: true),
                             textInputAction: TextInputAction.next,
-                              inputFormatters: [
-                                FilteringTextInputFormatter.allow(RegExp('[0-9.,]'))
-                              ],
+                            inputFormatters: [
+                              FilteringTextInputFormatter.allow(
+                                  RegExp('[0-9.-]'))
+                            ],
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Enter latitude.';
+                              }
+
+                              if (-90 <= double.parse(value) &&
+                                  double.parse(value) <= 90) {
+                                return 'Enter value between -90 and 90.';
+                              }
+
+                              return null;
+                            },
+                            onSaved: (value) {
+                              setState(() {
+                                _latitude = value.toString();
+                              });
+                            },
                             decoration: InputDecoration(
-                              labelText: 'Latitude',
-                              border: OutlineInputBorder()
-                              ),
+                                labelText: 'Latitude',
+                                border: OutlineInputBorder()),
                           ),
                         ),
                         SizedBox(
                           width: 20,
                           height: 20,
                         ),
-                        Flexible(
+                        Expanded(
                           child: TextFormField(
-                            keyboardType: TextInputType.numberWithOptions(decimal: true),
+                            keyboardType: TextInputType.numberWithOptions(
+                                signed: true, decimal: true),
                             textInputAction: TextInputAction.next,
                             inputFormatters: [
-                              FilteringTextInputFormatter.allow(RegExp('[0-9.,]'))
+                              FilteringTextInputFormatter.allow(
+                                  RegExp('[0-9.-]'))
                             ],
-                          decoration: InputDecoration(
-                            labelText: 'Longitude',
-                            border: OutlineInputBorder()
-                            ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Enter longitude.';
+                              }
+
+                              if (-180 <= double.parse(value) &&
+                                  double.parse(value) <= 180) {
+                                return 'Enter value between -90 and 90.';
+                              }
+
+                              return null;
+                            },
+                            onSaved: (value) {
+                              setState(() {
+                                _longitude = value.toString();
+                              });
+                            },
+                            decoration: InputDecoration(
+                                labelText: 'Longitude',
+                                border: OutlineInputBorder()),
                           ),
                         ),
                       ],
                     ),
-                  )
-                
-              ],
-            )),
+                    SizedBox(
+                      width: 20,
+                      height: 20,
+                    ),
+                    AspectRatio(
+                      aspectRatio: 1,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(20))),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.cloud_upload,
+                              color: Colors.white,
+                              size: 150,
+                            ),
+                            ElevatedButton(
+                              child: Text("Upload Image"),
+                              onPressed: () {},
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(onPressed: _onSubmit, child: Text('Submit'))
+                  ],
+                ),
+              )),
+        ),
       ),
     );
   }
